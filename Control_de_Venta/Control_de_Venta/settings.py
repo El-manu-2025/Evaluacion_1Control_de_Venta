75d43e8773471154671547c6bea1fb2848ad2e60
El-manu-2025 <manuel.ramos20@inacapmail.cl>
2025-10-30 13:01:43 -0300

    feat; render.yaml

--- Control_de_Venta/Control_de_Venta/settings.py
+++ Control_de_Venta/Control_de_Venta/settings.py
@@ -11,24 +11,29 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-# importar credenciales de postgresql
 import os
 from dotenv import load_dotenv
+import os
 load_dotenv()
 
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-$xmrdwnv%2&@#2w15vy@%&!o)#tkucm+!$8)s#@j=6e&i$g=1)'
 
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
 
-# Applicasiones instaladas
+# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -37,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.humanize',
     'tienda',
 ]
 
@@ -71,20 +75,23 @@ TEMPLATES = [
 WSGI_APPLICATION = 'Control_de_Venta.wsgi.application'
 
 
-
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-    "ENGINE": "django.db.backends.postgresql",
-    "NAME": os.getenv("DBNAME"),
-    "USER": os.getenv("USER"),
-    "PASSWORD": os.getenv("PASSWORD"),
-    "HOST": os.getenv("HOST"),
-    "PORT": os.getenv("PORT"),
-    }    
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('dbname'),
+        'USER': os.getenv('user'),
+        'PASSWORD': os.getenv('password'),
+        'HOST': os.getenv('host'),
+        'PORT': os.getenv('port'),
+    },
 }
 
 
+# Password validation
+# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -102,7 +109,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-
+# Internationalization
+# https://docs.djangoproject.com/en/5.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -113,8 +121,12 @@ USE_I18N = True
 USE_TZ = True
 
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
