7991e790149b1969ad5bf3f9a53b569464ec8feb
El-manu-2025 <manuel.ramos20@inacapmail.cl>
2025-10-30 22:53:58 -0300

    corregir rutas

--- Control_de_Venta/Control_de_Venta/settings.py
+++ Control_de_Venta/Control_de_Venta/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tienda',
+    'Control_de_Venta.tienda',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Control_de_Venta.urls'
+ROOT_URLCONF = 'Control_de_Venta.Control_de_Venta.urls'
+
 
 TEMPLATES = [
     {
