fa9aa5b04d6fe9a40862fa0dd20d36a617341e09
El-manu-2025 <manuel.ramos20@inacapmail.cl>
2025-10-31 23:32:32 -0300

    cambio del id del path urls

--- Control_de_Venta/tienda/urls.py
+++ Control_de_Venta/tienda/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.lista_productos, name='lista_productos'),
     path('agregar/', views.agregar_producto, name='agregar_producto'),
-    path('eliminar/<int:id>/', views.eliminar_producto, name='eliminar_producto'),
+    path('eliminar/<int:producto_id>/', views.eliminar_producto, name='eliminar_producto'),
     path('venta/', views.registrar_venta, name='registrar_venta'),
     path('resumen/', views.resumen_ventas, name='resumen_ventas'),
 ]
