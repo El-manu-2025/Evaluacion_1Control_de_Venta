254141b3cf3b623641903c76a679130bf2b6a060
El-manu-2025 <manuel.ramos20@inacapmail.cl>
2025-10-31 22:55:04 -0300

    corregi el name de apps tienda

--- Control_de_Venta/tienda/apps.py
+++ Control_de_Venta/tienda/apps.py
@@ -5,4 +5,5 @@ class TiendaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     # Declare the app name as the top-level 'tienda' package so Django
     # imports it from the project 'Control_de_Venta/tienda' directory.
-    name = 'tienda'
+    name = 'Control_de_Venta.tienda'
+
